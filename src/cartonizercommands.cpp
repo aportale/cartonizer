c2e6a5fcea06ce9686317ef7192955547a2b6f3b
Alessandro Portale <alessandro@casaportale.de>
2008-01-06 23:52:00 +0000

    Pretending to be a Software Architect (by adding commands and controller).

---
+++ src/cartonizercommands.cpp
@@ -0,0 +1,50 @@
+/*
+	Cartonizer - Box it beautifully!
+	Copyright (C) 2007-2008 by Alessandro Portale
+	http://cartonizer.sourceforge.net/
+
+	This file is part of Cartonizer
+
+	Cartonizer is free software; you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation; either version 2 of the License, or
+	(at your option) any later version.
+	
+	Cartonizer is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+	
+	You should have received a copy of the GNU General Public License
+	along with Cartonizer; if not, write to the Free Software
+	Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
+*/
+
+#include "cartonizercommands.h"
+
+CartonizerCommand::CartonizerCommand(QObject *cartonizer)
+{
+}
+
+PropertyCommand::PropertyCommand(QObject *cartonizer, const char *propertyName, const QVariant &propertyValue)
+	: CartonizerCommand(cartonizer)
+	, m_propertyName(propertyName)
+	, m_value(propertyValue)
+	, m_oldValue(cartonizer->property(propertyName))
+{
+}
+
+void PropertyCommand::undo()
+{
+	m_cartonizer->setProperty(m_propertyName, m_oldValue);
+}
+
+void PropertyCommand::redo()
+{
+	m_cartonizer->setProperty(m_propertyName, m_value);
+}
+
+int PropertyCommand::id() const
+{
+	return 10000 + m_cartonizer->metaObject()->indexOfProperty(m_propertyName);
+}
---
+++ src/cartonizercommands.h
@@ -0,0 +1,54 @@
+/*
+	Cartonizer - Box it beautifully!
+	Copyright (C) 2007-2008 by Alessandro Portale
+	http://cartonizer.sourceforge.net/
+
+	This file is part of Cartonizer
+
+	Cartonizer is free software; you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation; either version 2 of the License, or
+	(at your option) any later version.
+	
+	Cartonizer is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+	
+	You should have received a copy of the GNU General Public License
+	along with Cartonizer; if not, write to the Free Software
+	Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
+*/
+
+#ifndef CARTONIZERCOMMANDS_H
+#define CARTONIZERCOMMANDS_H
+
+#include <QUndoCommand>
+#include <QVariant>
+
+class CartonizerCommand : public QObject, public QUndoCommand
+{
+	Q_OBJECT
+
+public:
+	CartonizerCommand(QObject *cartonizer);
+
+protected:
+	QObject *m_cartonizer;
+};
+
+class PropertyCommand : public CartonizerCommand
+{
+public:
+	PropertyCommand(QObject *cartonizer, const char *propertyName, const QVariant &propertyValue);
+	void undo();
+	void redo();
+	int id() const;
+
+private:
+	const char *m_propertyName;
+	QVariant m_value;
+	QVariant m_oldValue;
+};
+
+#endif

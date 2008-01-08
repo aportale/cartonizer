ca6f3445cf6beaf6a9486b0735cb3c466d19f137
Alessandro Portale <alessandro@casaportale.de>
2008-01-08 03:57:06 +0000

    Added/corrected license comments

--- src/cartonizerpreviewwidget.h
+++ src/cartonizerpreviewwidget.h
@@ -1,3 +1,25 @@
+/*
+	Cartonizer - Box it beautifully!
+	Copyright (C) 2007-2008 by Alessandro Portale
+	http://cartonizer.sourceforge.net/
+
+	This file is part of Cartonizer
+
+	Cartonizer is free software; you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation; either version 2 of the License, or
+	(at your option) any later version.
+
+	Cartonizer is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with Cartonizer; if not, write to the Free Software
+	Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
+*/
+
 #ifndef CARTONIZERPREVIEWWIDGET_H
 #define CARTONIZERPREVIEWWIDGET_H
 

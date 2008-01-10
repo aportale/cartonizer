c793f37e2259423c8187fda6d5d1cf4293c5ea15
Alessandro Portale <alessandro@casaportale.de>
2008-01-10 16:40:32 +0000

    The properties

---
+++ src/cartonizerproperties.cpp
@@ -0,0 +1,8 @@
+#include "cartonizerproperties.h"
+
+const char* CartonizerProperties::xRotation = "xRotation";
+const char* CartonizerProperties::yRotation = "yRotation";
+const char* CartonizerProperties::observerHeight = "observerHeight";
+const char* CartonizerProperties::focalLength = "focalLength";
+const char* CartonizerProperties::specularityValue = "specularityValue";
+const char* CartonizerProperties::selectAndFocus = "selectAndFocus";
---
+++ src/cartonizerproperties.h
@@ -0,0 +1,15 @@
+#ifndef CARTONIZERPROPERTIES_H
+#define CARTONIZERPROPERTIES_H
+
+class CartonizerProperties {
+public:
+	static const char *xRotation;
+	static const char *yRotation;
+	static const char *observerHeight;
+	static const char *focalLength;
+	static const char *specularityValue;
+	static const char *selectAndFocus;
+};
+
+#endif
+

e6442ceb36824202d87a12704d3db9b034cad82d
Alessandro Portale <alessandro@casaportale.de>
2008-01-17 20:00:19 +0000

    Configuring Qt statically and stripped

---
+++ deployment/qt/qconfig-cartonizer.h
@@ -0,0 +1,115 @@
+
+/* Dialogs */
+#ifndef QT_NO_COLORDIALOG
+#  define QT_NO_COLORDIALOG
+#endif
+#ifndef QT_NO_FONTDIALOG
+#  define QT_NO_FONTDIALOG
+#endif
+#ifndef QT_NO_PRINTDIALOG
+#  define QT_NO_PRINTDIALOG
+#endif
+
+/* Images */
+#ifndef QT_NO_MOVIE
+#  define QT_NO_MOVIE
+#endif
+
+/* ItemViews */
+#ifndef QT_NO_DATAWIDGETMAPPER
+#  define QT_NO_DATAWIDGETMAPPER
+#endif
+#ifndef QT_NO_COLUMNVIEW
+#  define QT_NO_COLUMNVIEW
+#endif
+#ifndef QT_NO_PROXYMODEL
+#  define QT_NO_PROXYMODEL
+#endif
+#ifndef QT_NO_STRINGLISTMODEL
+#  define QT_NO_STRINGLISTMODEL
+#endif
+
+/* Kernel */
+#ifndef QT_NO_SOUND
+#  define QT_NO_SOUND
+#endif
+
+/* Painting */
+#ifndef QT_NO_COLORNAMES
+#  define QT_NO_COLORNAMES
+#endif
+#ifndef QT_NO_PRINTER
+#  define QT_NO_PRINTER
+#endif
+
+/* Styles */
+#ifndef QT_NO_STYLE_MOTIF
+#  define QT_NO_STYLE_MOTIF
+#endif
+#ifndef QT_NO_STYLE_CLEANLOOKS
+#  define QT_NO_STYLE_CLEANLOOKS
+#endif
+#ifndef QT_NO_STYLE_PLASTIQUE
+#  define QT_NO_STYLE_PLASTIQUE
+#endif
+#ifndef QT_NO_STYLE_STYLESHEET
+#  define QT_NO_STYLE_STYLESHEET
+#endif
+#ifndef QT_NO_STYLE_WINDOWSCE
+#  define QT_NO_STYLE_WINDOWSCE
+#endif
+#ifndef QT_NO_STYLE_WINDOWSMOBILE
+#  define QT_NO_STYLE_WINDOWSMOBILE
+#endif
+
+/* Utilities */
+#ifndef QT_NO_COMPLETER
+#  define QT_NO_COMPLETER
+#endif
+#ifndef QT_NO_SYSTEMTRAYICON
+#  define QT_NO_SYSTEMTRAYICON
+#endif
+
+/* Widgets */
+#ifndef QT_NO_LCDNUMBER
+#  define QT_NO_LCDNUMBER
+#endif
+#ifndef QT_NO_FONTCOMBOBOX
+#  define QT_NO_FONTCOMBOBOX
+#endif
+#ifndef QT_NO_CALENDARWIDGET
+#  define QT_NO_CALENDARWIDGET
+#endif
+#ifndef QT_NO_WORKSPACE
+#  define QT_NO_WORKSPACE
+#endif
+#ifndef QT_NO_SIGNALMAPPER
+#  define QT_NO_SIGNALMAPPER
+#endif
+#ifndef QT_NO_GRAPHICSVIEW
+#  define QT_NO_GRAPHICSVIEW
+#endif
+#ifndef QT_NO_MDIAREA
+#  define QT_NO_MDIAREA
+#endif
+#ifndef QT_NO_TEXTEDIT
+#  define QT_NO_TEXTEDIT
+#endif
+#ifndef QT_NO_SYNTAXHIGHLIGHTER
+#  define QT_NO_SYNTAXHIGHLIGHTER
+#endif
+#ifndef QT_NO_TEXTBROWSER
+#  define QT_NO_TEXTBROWSER
+#endif
+#ifndef QT_NO_SPLASHSCREEN
+#  define QT_NO_SPLASHSCREEN
+#endif
+#ifndef QT_NO_TABLEWIDGET
+#  define QT_NO_TABLEWIDGET
+#endif
+#ifndef QT_NO_TOOLBOX
+#  define QT_NO_TOOLBOX
+#endif
+#ifndef QT_NO_TREEWIDGET
+#  define QT_NO_TREEWIDGET
+#endif
